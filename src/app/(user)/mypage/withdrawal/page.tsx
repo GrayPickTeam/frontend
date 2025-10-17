7d231163a5a13382ad218baf269b0b45f1a87427
suhong99 <topic828@naver.com>
2025-10-17 19:03:15 +0900

    feat:프로필편집 및 회원탈퇴 상단에 되돌아가기 버튼 추가

--- src/app/(user)/mypage/withdrawal/page.tsx
+++ src/app/(user)/mypage/withdrawal/page.tsx
@@ -1,7 +1,10 @@
 import WithDrawalForm from '@/features/mypage/WithDrawalForm';
 import { COOKIE_NAME } from '@/shared/const/cookie';
+import { CLIENT_NAVI_PATH } from '@/shared/const/url';
+import BackIcon from '@/shared/icon/Back';
 import { Metadata } from 'next';
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
 	title: '회원 탈퇴',
@@ -12,6 +15,10 @@ export default async function withDrawal() {
 	const nickname = cookieStore.get(COOKIE_NAME.auth.nickname)?.value;
 	return (
 		<div className="flex flex-col max-w-[480px] w-full justify-center px-5 py-12 desktop:py-15 gap-6 mx-auto desktop:gap-10">
+			<Link href={CLIENT_NAVI_PATH.myprofile.path} className="flex items-center py-1 gap-1">
+				<BackIcon />
+				<span className="typo-label2 font-bold text-label-alternative">프로필 편집으로 돌아가기</span>
+			</Link>
 			<p className="flex flex-col  gap-5 w-full">
 				<span className="typo-heading1 font-bold text-label-normal">{nickname}님과 마지막이라니 아쉬워요</span>
 				<span className="typo-body1-normal text-label-neutral font-regular">
